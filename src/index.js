1cbc19a20b349217481ba0ceef3e03fa24448b2d
JisanAR03 <altafurrahaman3@gmail.com>
2024-01-31 06:06:54 +0600

    adding feature

--- src/index.js
+++ src/index.js
@@ -32,12 +32,16 @@ const run = async () => {
             if (assignedIssue.number === issue.number) {
                 continue;
             }
-    
-            // Check for pull requests that mention the assigned issue
-            const pullRequests = await octokit.paginate(octokit.search.issuesAndPullRequests, {
+            pullRequests = await octokit.paginate(octokit.search.issuesAndPullRequests, {
                 q: `is:pr is:open repo:${owner}/${repo} linked:issue ${assignedIssue.number}`
             });
-            console.log(pullRequests);
+            console.log("pullRequests", pullRequests);
+            console.log("pullRequests.total_count", pullRequests.total_count);
+            // Check for pull requests that mention the assigned issue
+            // const pullRequests = await octokit.paginate(octokit.search.issuesAndPullRequests, {
+            //     q: `is:pr is:open repo:${owner}/${repo} linked:issue ${assignedIssue.number}`
+            // });
+            // console.log(pullRequests);
             // If there are no linked pull requests for this issue, add it to the list
             if (pullRequests.total_count === 0) {
                 issuesWithoutPR.push(assignedIssue.number);
