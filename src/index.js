16140a10a5efd104cac58cae872d1051d9e73880
Krrish Sehgal <133865424+krrish-sehgal@users.noreply.github.com>
2025-04-03 11:27:09 +0530

    sender verified

--- src/index.js
+++ src/index.js
@@ -183,7 +183,7 @@ const run = async () => {
 
                     try {
                         // Send kudos to the API
-                        await axios.post('https://owaspblt.com/teams/give-kudos/', {
+                        await axios.post('https://c94a-2409-40d0-b-b1e0-a4a5-1621-e8af-d0eb.ngrok-free.app/teams/give-kudos/', {
                             kudosReceiver: receiver,
                             kudosSender: sender,
                             comment: kudosComment
