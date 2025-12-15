8bc0752028e71174798f9a6c6356f9521b2c85f3
Jayant Malvi <cs24b077@smail.iitm.ac.in>
2025-12-16 02:29:36 +0530

    Added docs for utils.py

--- dist/index.js
+++ dist/index.js
@@ -36558,6 +36558,11 @@ function wrappy (fn, cb) {
 /***/ 5804:
 /***/ ((module) => {
 
+/**
+ * Determines if a comment was made by a human user.
+ * @param {object} comment - GitHub comment object
+ * @returns {boolean} true if commenter is a User or Mannequin (imported account)
+ */
 function isHumanCommenter(comment) {
     return (
         comment &&
@@ -36566,6 +36571,11 @@ function isHumanCommenter(comment) {
     );
 }
 
+/**
+ * Safely extracts user login and type from a comment.
+ * @param {object} comment - GitHub comment object
+ * @returns {{login: string, type: string}} user info with "unknown" defaults
+ */
 function extractUserInfo(comment) {
     const login = comment?.user?.login ?? "unknown";
     const type = comment?.user?.type ?? "unknown";
--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,8 @@
+/**
+ * Determines if a comment was made by a human user.
+ * @param {object} comment - GitHub comment object
+ * @returns {boolean} true if commenter is a User or Mannequin (imported account)
+ */
 function isHumanCommenter(comment) {
     return (
         comment &&
@@ -6,6 +11,11 @@ function isHumanCommenter(comment) {
     );
 }
 
+/**
+ * Safely extracts user login and type from a comment.
+ * @param {object} comment - GitHub comment object
+ * @returns {{login: string, type: string}} user info with "unknown" defaults
+ */
 function extractUserInfo(comment) {
     const login = comment?.user?.login ?? "unknown";
     const type = comment?.user?.type ?? "unknown";
